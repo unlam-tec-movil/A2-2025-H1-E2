013038714bd12f460828b12f2deb6db940d623e1
Kajukai32 <arturojas32@gmail.com>
2025-05-28 22:24:25 -0300

    Screen de crear post, falta pasarle el viewmodel como argumento y las url de la data para las miniaturas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     defaultConfig {
         applicationId = "ar.edu.unlam.mobile.scaffolding"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
