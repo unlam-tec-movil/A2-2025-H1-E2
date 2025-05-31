1dd53bd596b34a519a735e442077d1cb0181a81a
Juliana <jfsamaniego@alumno.unlam.edu.ar>
2025-05-31 19:02:58 -0300

    Ktlin fix

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/TuitView.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/TuitView.kt
@@ -22,62 +22,67 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import models.Tuit
 import coil.compose.rememberAsyncImagePainter
+import models.Tuit
 
 @Composable
 fun TuitView(tuit: Tuit) {
     Row(
         horizontalArrangement = Arrangement.spacedBy(4.dp),
-        modifier = Modifier
-            .background(Color.White)
-            .padding(4.dp),
+        modifier =
+            Modifier
+                .background(Color.White)
+                .padding(4.dp),
     ) {
         if (tuit.avatarUrl.isNotEmpty()) {
             Image(
                 painter = rememberAsyncImagePainter(tuit.avatarUrl),
                 contentDescription = null,
-                modifier = Modifier
-                    .size(20.dp)
-                    .clip(CircleShape)
-                    .background(Color.Gray)
+                modifier =
+                    Modifier
+                        .size(20.dp)
+                        .clip(CircleShape)
+                        .background(Color.Gray),
             )
         }
 
         Column(
-            verticalArrangement = Arrangement.spacedBy(4.dp)
+            verticalArrangement = Arrangement.spacedBy(4.dp),
         ) {
             Row(
                 verticalAlignment = Alignment.Bottom,
-                horizontalArrangement = Arrangement.spacedBy(4.dp)
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
             ) {
-                Text(text = tuit.author,
+                Text(
+                    text = tuit.author,
                     fontSize = 10.sp,
                     fontWeight = FontWeight.Bold,
-                    modifier = Modifier.alignByBaseline()
+                    modifier = Modifier.alignByBaseline(),
                 )
 
                 Text(
                     text = tuit.date,
                     fontSize = 6.sp,
                     color = Color.Gray,
-                    modifier = Modifier.alignByBaseline()
+                    modifier = Modifier.alignByBaseline(),
                 )
             }
 
-            Text(text = tuit.message,
-                fontSize = 10.sp)
+            Text(
+                text = tuit.message,
+                fontSize = 10.sp,
+            )
 
             Row(horizontalArrangement = Arrangement.spacedBy(2.dp)) {
                 Icon(
                     imageVector = if (tuit.liked) Icons.Filled.Favorite else Icons.Filled.FavoriteBorder,
                     contentDescription = "Likes",
                     tint = if (tuit.liked) Color.Red else Color.Black,
-                    modifier = Modifier.size(12.dp)
+                    modifier = Modifier.size(12.dp),
                 )
                 Text(
                     text = tuit.likes.toString(),
-                    fontSize = 10.sp
+                    fontSize = 10.sp,
                 )
             }
         }
@@ -87,15 +92,16 @@ fun TuitView(tuit: Tuit) {
 @Preview
 @Composable
 fun TuitViewPreview() {
-    val tuit = Tuit(
-        id = 1,
-        message = "This is a sample tuit message.",
-        parentId = 0,
-        author = "John Doe",
-        avatarUrl = "https://fotos.perfil.com/2023/06/13/trim/720/410/messi-copa-del-mundo-1588008.jpg?webp",
-        likes = 10,
-        liked = false,
-        date = "2023-10-01"
-    )
+    val tuit =
+        Tuit(
+            id = 1,
+            message = "This is a sample tuit message.",
+            parentId = 0,
+            author = "John Doe",
+            avatarUrl = "https://fotos.perfil.com/2023/06/13/trim/720/410/messi-copa-del-mundo-1588008.jpg?webp",
+            likes = 10,
+            liked = false,
+            date = "2023-10-01",
+        )
     TuitView(tuit)
 }
